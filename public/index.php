304b5b9e9cc9d0ad0fafc28ff57f40184d10e84d
Smart4U <inbox.martinez.dev@gmail.com>
2017-08-07 20:14:59 +0200

    Adding request and response with PSR-7

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php declare(strict_types=1);
 
+use function Http\Response\send;
+
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\ServerRequest;
+
+
 /**
  * Front controller
  *
@@ -18,5 +24,12 @@ define('STORAGE',  BASE.'storage/');
  * Bootstrap file to load the composer autoloader
  */
 require BASE.'bootstrap.php';
+
+
+$request = ServerRequest::fromGlobals();
+$response = new Response();
+$response->getBody()->write('homepage');
+
+send($response);
 
 ?>
