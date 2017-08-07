700d9159ac0feee5477f07a05487c19ba014044e
Smart4U <inbox.martinez.dev@gmail.com>
2017-08-07 19:15:43 +0200

    Add PSR-4 composer autoloading

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+/**
+ * Front controller
+ *
+ * PHP version 7.1
+ */
+
+
+/**
+ *  constants
+ */
+define('BASE',     dirname(__DIR__).'/');
+define('CONFIG',   BASE.'config/');
+define('STORAGE',  BASE.'storage/');
+
+/**
+ * Bootstrap file to load the composer autoloader
+ */
+require BASE.'vendor/autoload.php';
+
+?>
