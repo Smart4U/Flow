a86148a2dbdfa4af4a54dc6a7b2213acfa17fa66
Smart4U <inbox.martinez.dev@gmail.com>
2017-08-11 00:59:04 +0200

    Add whoops handler error

--- bootstrap.php
+++ bootstrap.php
@@ -4,6 +4,14 @@
 require BASE . '/vendor/autoload.php';
 
 /**
+ * whoops is an error handler framework for PHP
+ */
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
+
+/**
  * The Dotenv Component parses .env files to make environment
  * variables stored in them accessible via getenv()
  */
